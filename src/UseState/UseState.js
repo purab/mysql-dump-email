1750a9b50521c818e2d050d1e059499dbb69b672
Purab <purabdk@yahoo.co.in>
2021-10-10 21:43:42 +0530

    adding usestate..example

--- src/UseState/UseState.js
+++ src/UseState/UseState.js
@@ -2,14 +2,22 @@ import React, {useState} from 'react';
 
 const UseState = () => {
     const [counter, setCounter] = useState(0);
+    const [inputValue, setInputValue] = useState("petro")
     
     const increment = () => {
         setCounter(counter+1)
+    }
+    let onChangeHandle = (e) => {
+        const newValue = e.target.value;
+        setInputValue(newValue);
     }
     return (
         <div>
             {counter}
             <button onClick={increment}>increment</button>
+            <br />
+            <input placeholder="enter something here..." onChange={onChangeHandle}/>
+            {inputValue}
         </div>
     );    
 }
